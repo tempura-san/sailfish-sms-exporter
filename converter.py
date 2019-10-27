9f1785f34ee00549831c3e62aea6fdca0e92c9ea
Tempura San <tempura.san@gmail.com>
2019-10-27 20:09:37 +0100

    SMS: use lxml instead of simple text XML output
    remove optional attributes

--- converter.py
+++ converter.py
@@ -6,27 +6,16 @@ Convert sailfish sms and call history to xml to be imported to android phones.
 Reads a sailfish commhistory.db (which can be found in `/home/nemo/.local/share/commhistory`)
 and creates a xml-file of all contained sms that is understood by the Android Application
 "SMS Backup and Restore" (which can be obtained here:
-https://play.google.com/store/apps/details?id=com.riteshsahu.SMSBackupRestore)
+<https://play.google.com/store/apps/details?id=com.riteshsahu.SMSBackupRestore>).
 """
 
 import sqlite3
 from datetime import datetime
+from lxml import etree as ET
 
 jolla_db_filename = 'commhistory.db'
 sms_filename = 'sms.xml'
 calls_filename = 'calls.xml'
-
-
-def encode_xml(text):
-    """Escape the text for xml."""
-    if not text:
-        return ""
-    text = text.replace("&", "&amp;")
-    text = text.replace("\"", "&quot;")
-    text = text.replace("'", "&apos;")
-    text = text.replace("<", "&lt;")
-    text = text.replace(">", "&gt;")
-    return text.encode('utf-8')
 
 cols = ["id", "type", "startTime", "endTime", "direction", "isDraft", "isRead", "isMissedCall", "isEmergencyCall", "status", "bytesReceived", "localUid", "remoteUid", "parentId", "subject", "freeText", "groupId", "messageToken", "lastModified", "vCardFileName", "vCardLabel", "isDeleted", "reportDelivery", "validityPeriod", "contentLocation", "messageParts", "headers", "readStatus", "reportRead", "reportedReadRequested", "mmsId", "isAction", "hasExtraProperties", "hasMessageParts"]
 
@@ -37,53 +26,27 @@ entries = cursor.fetchall()
 
 date = datetime.now()
 
-out = """<?xml version='1.0' encoding='UTF-8' standalone='yes' ?>
-<!--File Created on {date}-->
-<?xml-stylesheet type="text/xsl" href="sms.xsl"?>
-<smses count="{count}">""".format(count=len(entries), date=date.strftime("%d/%m/%y %H:%M:%S"))
+outxml = ET.Element('smses')
+outxml.set('count', str(len(entries)))
 
 for entry in entries:
     direction = entry[cols.index('direction')]
-    # if direction == 2 and not entry[cols.index('remoteUid')]:
-    #     continue
     sentdate = datetime.fromtimestamp(entry[cols.index('startTime')])
-    out += """<sms
-    protocol="{protocol}"
-    address="{address}"
-    date="{date}"
-    type="{type}"
-    subject="{subject}"
-    body="{body}"
-    toa="{toa}"
-    sc_toa="{sc_toa}"
-    service_center="{service_center}"
-    read="{read}"
-    status="{status}"
-    locked="{locked}"
-    date_sent="{date_sent}"
-    readable_date="{readable_date}"
-    contact_name="{contact_name}" />\n""".format(
-        protocol=0,
-        address=entry[cols.index('remoteUid')],
-        date=entry[cols.index('startTime')] * 1000,
-        type=direction,
-        subject=entry[cols.index('subject')] or "null",
-        body=encode_xml(entry[cols.index('freeText')]),
-        toa="null",
-        sc_toa="null",
-        service_center="null",
-        read=1,
-        status=-1,
-        locked=0,
-        date_sent=(0 if direction == 2 else entry[cols.index('startTime')] * 1000),
-        readable_date=sentdate.strftime("%d.%m.%Y %H:%M:%S"),
-        contact_name=""
-    )
 
-out += "\n</smses>"
+    # a description of the used XML format can be found here:
+    # https://synctech.com.au/sms-backup-restore/fields-in-xml-backup-files/
+    sms = ET.SubElement(outxml, 'sms')
+    sms.set('protocol', "0") # 0 = SMS
+    sms.set('address', str(entry[cols.index('remoteUid')])) # phone number
+    sms.set('date', str(entry[cols.index('startTime')] * 1000)) # date sent/received
+    sms.set('type', str(direction)) # 1..received, 2..sent
+    sms.set('subject', entry[cols.index('subject')] or "null") # usually "null" for SMS
+    sms.set('body', entry[cols.index('freeText')] or "") # message content
+    sms.set('read', str(entry[cols.index('isRead')])) # assume all messages are read
+    sms.set('status', "-1") # Status = None
 
-with open(sms_filename, 'w') as fp:
-    fp.write(out)
+with open(sms_filename, 'wb') as fp:
+    fp.write(ET.tostring(outxml, encoding='utf8', method='xml', pretty_print=True))
 
 cursor = connection.cursor()
 cursor.execute("SELECT * FROM Events WHERE type=3")
